839db700f5eb646858fd5a67106cbd8145aa3d8f
Erica <erica.abdao@zup.com.br>
2024-12-13 16:19:56 -0300

    feat: Incluir função de escolher dificuldade(fácil, média e difícil).

---
+++ src/JogoAdivinha.java
@@ -0,0 +1,124 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class JogoAdivinha {
+    public static void adivinha(int dificuldade) {
+        Random random = new Random();
+        int sorteiaNumero = random.nextInt(dificuldade);
+        System.out.println(sorteiaNumero);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Tente acertar meu número, digite o seu: ");
+        int numero = Integer.parseInt(scanner.nextLine());
+
+        int pontuacao = 0;
+
+        while (pontuacao<99){
+            if (sorteiaNumero == numero) {
+                pontuacao = pontuacao + 10;
+                System.out.println("Parabéns você ganhou 10 pontos. Sua pontuação atual é: " + pontuacao);
+
+                if (pontuacao >= 100) {
+                    System.out.println("Parabéns você ganhou");
+                } else {
+                    Scanner continua = new Scanner(System.in);
+
+                    System.out.println("Deseja continuar? Digite 1 para sim ou 2 para não: ");
+                    int continuar = Integer.parseInt(continua.nextLine());
+
+                    if (continuar == 1) {
+                        Random random2 = new Random();
+                        sorteiaNumero = random2.nextInt(dificuldade);
+
+                        Scanner scanner2 = new Scanner(System.in);
+
+                        System.out.print("Tente novamente, digite outro número: ");
+                        numero = Integer.parseInt(scanner2.nextLine());
+                    } else if (continuar == 2) {
+                        break;
+                    } else {
+                        System.out.println("Opção inválida, escolha uma opção válida. ");
+
+                        System.out.println("Deseja continuar? Digite 1 para sim ou 2 para não: ");
+                        continuar = Integer.parseInt(continua.nextLine());
+                        if (continuar == 1) {
+                            Scanner scanner6 = new Scanner(System.in);
+
+                            System.out.print("Tente novamente, digite outro número: ");
+                            numero = Integer.parseInt(scanner6.nextLine());
+                        } else if (continuar == 2){
+                            break;
+                        }
+
+                    }
+                }
+
+            } else if (sorteiaNumero == numero - 1 || sorteiaNumero == numero + 1) {
+                pontuacao = pontuacao + 5;
+                System.out.println("Parabéns você ganhou 5 pontos. Sua pontuação atual é: " + pontuacao);
+
+                Scanner continua = new Scanner(System.in);
+
+                System.out.println("Deseja continuar? Digite 1 para sim ou 2 para não: ");
+                int continuar = Integer.parseInt(continua.nextLine());
+
+                if (continuar == 1) {
+                    if (pontuacao >= 100) {
+                        System.out.println("Parabéns você ganhou");
+                    } else {
+                        Scanner scanner3 = new Scanner(System.in);
+
+                        System.out.print("Tente novamente, digite outro número: ");
+                        numero = Integer.parseInt(scanner3.nextLine());
+                    }
+                    //continuar = 0;
+                } else if (continuar == 2) {
+                    break;
+                } else {
+                    System.out.println("Opção inválida, escolha uma opção válida. ");
+
+                    System.out.println("Deseja continuar? Digite 1 para sim ou 2 para não: ");
+                    continuar = Integer.parseInt(continua.nextLine());
+                    if (continuar == 1) {
+                        Scanner scanner6 = new Scanner(System.in);
+
+                        System.out.print("Tente novamente, digite outro número: ");
+                        numero = Integer.parseInt(scanner6.nextLine());
+                    } else if (continuar == 2){
+                        break;
+                    }
+                }
+            } else {
+                System.out.println("Que pena você não pontuou. Sua pontuação atual é: " + pontuacao);
+
+                Scanner continua = new Scanner(System.in);
+
+                System.out.println("Deseja continuar? Digite 1 para sim ou 2 para não: ");
+                int continuar = Integer.parseInt(continua.nextLine());
+
+                if (continuar == 1) {
+                    Scanner scanner4 = new Scanner(System.in);
+
+                    System.out.print("Tente novamente, digite outro número: ");
+                    numero = Integer.parseInt(scanner4.nextLine());
+                    //continuar = 0;
+                } else if (continuar == 2) {
+                    break;
+                } else {
+                    System.out.println("Opção inválida, escolha uma opção válida. ");
+
+                    System.out.println("Deseja continuar? Digite 1 para sim ou 2 para não: ");
+                    continuar = Integer.parseInt(continua.nextLine());
+                    if (continuar == 1) {
+                        Scanner scanner6 = new Scanner(System.in);
+
+                        System.out.print("Tente novamente, digite outro número: ");
+                        numero = Integer.parseInt(scanner6.nextLine());
+                    } else if (continuar == 2){
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,128 +3,43 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Random random = new Random();
-        int sorteiaNumero = random.nextInt(10);
+            int dif = 0;
+            int escolha = 0;
+            Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
+            System.out.println("Escolha qual dificuldade você vai querer: 1.Fácil, 2.Médio, 3.Difícil");
+            escolha = Integer.parseInt(scanner.nextLine());
 
-        System.out.print("Tente acertar meu número, digite o seu: ");
-        int numero = Integer.parseInt(scanner.nextLine());
+            if(escolha ==1) {
+                dif = 10;
+            }
+            else if(escolha ==2){
+                dif = 50;
+            }
+            else if(escolha ==3) {
+                dif = 100;
+            }
+            else {
+                System.out.println("Opção inválida, digite uma opção válida!");
 
-        int pontuacao = 0;
-
-        while (pontuacao<99){
-            if (sorteiaNumero == numero) {
-                pontuacao = pontuacao + 10;
-                System.out.println("Parabéns você ganhou 10 pontos. Sua pontuação atual é: " + pontuacao);
-
-                if (pontuacao >= 100) {
-                    System.out.println("Parabéns você ganhou");
+                System.out.println("Escolha qual dificuldade você vai querer: 1.Fácil, 2.Médio, 3.Difícil");
+                escolha = Integer.parseInt(scanner.nextLine());
+                if (escolha == 1) {
+                    dif = 10;
+                } else if (escolha == 2) {
+                    dif = 50;
+                } else if (escolha == 3) {
+                    dif = 100;
                 } else {
-                    Scanner continua = new Scanner(System.in);
-
-                    System.out.println("Deseja continuar? Digite 1 para sim ou 2 para não: ");
-                    int continuar = Integer.parseInt(continua.nextLine());
-
-                        if (continuar == 1) {
-                            Random random2 = new Random();
-                            sorteiaNumero = random2.nextInt(10);
-
-                            Scanner scanner2 = new Scanner(System.in);
-
-                            System.out.print("Tente novamente, digite outro número: ");
-                            numero = Integer.parseInt(scanner2.nextLine());
-//                            continuar = 0;
-
-                        } else if (continuar == 2) {
-                            break;
-                        } else {
-                            System.out.println("Opção inválida, escolha uma opção válida. ");
-
-                            System.out.println("Deseja continuar? Digite 1 para sim ou 2 para não: ");
-                            continuar = Integer.parseInt(continua.nextLine());
-                            if (continuar == 1) {
-                                Scanner scanner6 = new Scanner(System.in);
-
-                                System.out.print("Tente novamente, digite outro número: ");
-                                numero = Integer.parseInt(scanner6.nextLine());
-                            } else if (continuar == 2){
-                                break;
-                            }
-
-                        }
+                    return;
                 }
-
-            } else if (sorteiaNumero == numero - 1 || sorteiaNumero == numero + 1) {
-                pontuacao = pontuacao + 5;
-                System.out.println("Parabéns você ganhou 5 pontos. Sua pontuação atual é: " + pontuacao);
-
-                Scanner continua = new Scanner(System.in);
-
-                System.out.println("Deseja continuar? Digite 1 para sim ou 2 para não: ");
-                int continuar = Integer.parseInt(continua.nextLine());
-
-                if (continuar == 1) {
-                    if (pontuacao >= 100) {
-                        System.out.println("Parabéns você ganhou");
-                    } else {
-                        Scanner scanner3 = new Scanner(System.in);
-
-                        System.out.print("Tente novamente, digite outro número: ");
-                        numero = Integer.parseInt(scanner3.nextLine());
-                    }
-                    //continuar = 0;
-                } else if (continuar == 2) {
-                    break;
-                } else {
-                    System.out.println("Opção inválida, escolha uma opção válida. ");
-
-                    System.out.println("Deseja continuar? Digite 1 para sim ou 2 para não: ");
-                    continuar = Integer.parseInt(continua.nextLine());
-                    if (continuar == 1) {
-                        Scanner scanner6 = new Scanner(System.in);
-
-                        System.out.print("Tente novamente, digite outro número: ");
-                        numero = Integer.parseInt(scanner6.nextLine());
-                    } else if (continuar == 2){
-                        break;
-                    }
-                }
-            } else {
-                System.out.println("Que pena você não pontuou. Sua pontuação atual é: " + pontuacao);
-
-                Scanner continua = new Scanner(System.in);
-
-                System.out.println("Deseja continuar? Digite 1 para sim ou 2 para não: ");
-                int continuar = Integer.parseInt(continua.nextLine());
-
-                if (continuar == 1) {
-                    Scanner scanner4 = new Scanner(System.in);
-
-                    System.out.print("Tente novamente, digite outro número: ");
-                    numero = Integer.parseInt(scanner4.nextLine());
-                    //continuar = 0;
-                } else if (continuar == 2) {
-                    break;
-                } else {
-                    System.out.println("Opção inválida, escolha uma opção válida. ");
-
-                    System.out.println("Deseja continuar? Digite 1 para sim ou 2 para não: ");
-                    continuar = Integer.parseInt(continua.nextLine());
-                    if (continuar == 1) {
-                        Scanner scanner6 = new Scanner(System.in);
-
-                        System.out.print("Tente novamente, digite outro número: ");
-                        numero = Integer.parseInt(scanner6.nextLine());
-                    } else if (continuar == 2){
-                        break;
-                    }
-                }
-
-
-
 
             }
-        }
-        }
+            System.out.println("dif: " + dif + ", escolha: "+ escolha);
+
+            JogoAdivinha.adivinha(dif);
+
+
+
     }
+}
