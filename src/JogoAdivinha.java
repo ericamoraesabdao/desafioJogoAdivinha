975e54d5c2f76205f130afcaa712fa1fd87b2594
Erica <erica.abdao@zup.com.br>
2024-12-16 14:16:54 -0300

    feat: Incluir lista de números acertados e números errados ao final do jogo.

--- src/JogoAdivinha.java
+++ src/JogoAdivinha.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,15 +12,23 @@ public class JogoAdivinha {
         System.out.print("Tente acertar meu número, digite o seu: ");
         int numero = Integer.parseInt(scanner.nextLine());
 
+        ArrayList numerosAcertados = new ArrayList();
+        ArrayList numerosErrados = new ArrayList();
+
         int pontuacao = 0;
 
         while (pontuacao<99){
             if (sorteiaNumero == numero) {
                 pontuacao = pontuacao + 10;
                 System.out.println("Parabéns você ganhou 10 pontos. Sua pontuação atual é: " + pontuacao);
+                numerosAcertados.add(numero);
+                System.out.println("acertos" + numerosAcertados);
 
                 if (pontuacao >= 100) {
-                    System.out.println("Parabéns você ganhou");
+                    System.out.println("Parabéns você chegou a 100 pontos e ganhou");
+                    System.out.println("Números acertados: " + numerosAcertados);
+                    System.out.println("Números errados: " + numerosErrados);
+
                 } else {
                     Scanner continua = new Scanner(System.in);
 
@@ -35,7 +44,11 @@ public class JogoAdivinha {
                         System.out.print("Tente novamente, digite outro número: ");
                         numero = Integer.parseInt(scanner2.nextLine());
 
+
                     } else if (continuar == 2) {
+                        System.out.println("Você resolveu parar com " + pontuacao + " pontos. Você não ganhou!");
+                        System.out.println("Você acertou os números: " + numerosAcertados);
+                        System.out.println("Você errou os números: " + numerosErrados);
                         break;
                     } else {
                         System.out.println("Opção inválida, escolha uma opção válida. ");
@@ -47,7 +60,11 @@ public class JogoAdivinha {
 
                             System.out.print("Tente novamente, digite outro número: ");
                             numero = Integer.parseInt(scanner6.nextLine());
+
                         } else if (continuar == 2){
+                            System.out.println("Você resolveu parar com " + pontuacao + " pontos. Você não ganhou!");
+                            System.out.println("Você acertou os números: " + numerosAcertados);
+                            System.out.println("Você errou os números: " + numerosErrados);
                             break;
                         }
 
@@ -57,6 +74,8 @@ public class JogoAdivinha {
             } else if (sorteiaNumero == numero - 1 || sorteiaNumero == numero + 1) {
                 pontuacao = pontuacao + 5;
                 System.out.println("Parabéns você ganhou 5 pontos. Sua pontuação atual é: " + pontuacao);
+                numerosErrados.add(numero);
+                System.out.println("erros" + numerosErrados);
 
                 Scanner continua = new Scanner(System.in);
 
@@ -65,15 +84,19 @@ public class JogoAdivinha {
 
                 if (continuar == 1) {
                     if (pontuacao >= 100) {
-                        System.out.println("Parabéns você ganhou");
+                        System.out.println("Parabéns você chegou a 100 pontos e ganhou");
+                        System.out.println("Números acertados: " + numerosAcertados);
+                        System.out.println("Números errados: " + numerosErrados);
                     } else {
                         Scanner scanner3 = new Scanner(System.in);
 
                         System.out.print("Tente novamente, digite outro número: ");
                         numero = Integer.parseInt(scanner3.nextLine());
                     }
-                    //continuar = 0;
                 } else if (continuar == 2) {
+                    System.out.println("Você resolveu parar com " + pontuacao + " pontos. Você não ganhou!");
+                    System.out.println("Você acertou os números: " + numerosAcertados);
+                    System.out.println("Você errou os números: " + numerosErrados);
                     break;
                 } else {
                     System.out.println("Opção inválida, escolha uma opção válida. ");
@@ -86,11 +109,16 @@ public class JogoAdivinha {
                         System.out.print("Tente novamente, digite outro número: ");
                         numero = Integer.parseInt(scanner6.nextLine());
                     } else if (continuar == 2){
+                        System.out.println("Você resolveu parar com " + pontuacao + " pontos. Você não ganhou!");
+                        System.out.println("Você acertou os números: " + numerosAcertados);
+                        System.out.println("Você errou os números: " + numerosErrados);
                         break;
                     }
                 }
             } else {
                 System.out.println("Que pena você não pontuou. Sua pontuação atual é: " + pontuacao);
+                numerosErrados.add(numero);
+                System.out.println("erros" + numerosErrados);
 
                 Scanner continua = new Scanner(System.in);
 
@@ -102,8 +130,10 @@ public class JogoAdivinha {
 
                     System.out.print("Tente novamente, digite outro número: ");
                     numero = Integer.parseInt(scanner4.nextLine());
-                    //continuar = 0;
                 } else if (continuar == 2) {
+                    System.out.println("Você resolveu parar com " + pontuacao + " pontos. Você não ganhou!");
+                    System.out.println("Você acertou os números: " + numerosAcertados);
+                    System.out.println("Você errou os números: " + numerosErrados);
                     break;
                 } else {
                     System.out.println("Opção inválida, escolha uma opção válida. ");
@@ -116,6 +146,9 @@ public class JogoAdivinha {
                         System.out.print("Tente novamente, digite outro número: ");
                         numero = Integer.parseInt(scanner6.nextLine());
                     } else if (continuar == 2){
+                        System.out.println("Você resolveu parar com " + pontuacao + " pontos. Você não ganhou!");
+                        System.out.println("Você acertou os números: " + numerosAcertados);
+                        System.out.println("Você errou os números: " + numerosErrados);
                         break;
                     }
                 }
